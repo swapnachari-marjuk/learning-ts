0fa220fdc58c5342f074d2dc6b8141a7aa6acef2
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2026-01-07 20:08:25 +0600

    learned about primary typing and functions typing.

--- hello-ts/public/js/index.js
+++ hello-ts/public/js/index.js
@@ -1,6 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let firstName = "Ahmad ";
-const lastName = "Marjuk";
-firstName = 3;
+let firstName = "Ahmad "; //example of implicit typing or Type Inference.
+const lastName = "Marjuk"; // example of explicit typing.
+// typescript primary types are: string, number, boolian...
+// firstName = 3 this will be an error in typescript.
+const greets = (name, age) => {
+    const greetsSent = `Hello! I'm ${name}. I'm ${age} years old.`;
+    return greetsSent;
+};
+const funCall = greets("Ahmad Marjuk", 23);
+console.log(funCall);
+// In the case of a function in typescirpt, the data type of the argument must be specified. 
+// If desired, you can also specify what it will return.
+// when a function returning nothing, its type will be `void`. But it can run an operation.
+// if it typed `never` it will return nothing and it can't run any operation
 //# sourceMappingURL=index.js.map
--- hello-ts/src/index.ts
+++ hello-ts/src/index.ts
@@ -1,4 +1,18 @@
-let firstName = "Ahmad "
-const lastName = "Marjuk"
+let firstName = "Ahmad "; //example of implicit typing or Type Inference.
+const lastName: string = "Marjuk"; // example of explicit typing.
 
-firstName = 3
+// typescript primary types are: string, number, boolian...
+// firstName = 3 this will be an error in typescript.
+
+const greets = (name: string, age: number): string => {
+  const greetsSent = `Hello! I'm ${name}. I'm ${age} years old.`;
+  return greetsSent;
+};
+
+const funCall = greets("Ahmad Marjuk", 23);
+console.log(funCall);
+
+// In the case of a function in typescirpt, the data type of the argument must be specified. 
+// If desired, you can also specify what it will return.
+// when a function returning nothing, its type will be `void`. But it can run an operation.
+// if it typed `never` it will return nothing and it can't run any operation
