e19fc11f09ad6af3ee366fbf33563ac92f97aa0f
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2026-01-09 22:15:57 +0600

    learned basic objects.

--- hello-ts/public/js/index.js
+++ hello-ts/public/js/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let firstName = "Ahmad "; //example of implicit typing or Type Inference.
 const lastName = "Marjuk"; // example of explicit typing.
 // typescript primary types are: string, number, boolian...
-// firstName = 3 this will be an error in typescript.
+// firstName = 3; this will be an error in typescript.
 const greets = (name, age) => {
     const greetsSent = `Hello! I'm ${name}. I'm ${age} years old.`;
     return greetsSent;
@@ -13,7 +13,14 @@ console.log(funCall);
 // In the case of a function in typescirpt, the data type of the argument must be specified.
 // If desired, you can also specify what it will return.
 // when a function returning nothing, its type will be `void`. But it can run an operation.
+const voidGreets = (name, age) => {
+    const greetsSent = `Hello! I'm ${name}. I'm ${age} years old.`;
+    console.log(greetsSent);
+};
 // if it typed `never` it will return nothing and it can't run any operation
+function error(message) {
+    throw new Error(message);
+}
 // const foods = ["potato", "tomato", "shim", "fish",]
 // foods.push(39) when foods is implicitly string typed, you can't push any data accept string.
 const foods = ["potato", "tomato", "shim", "fish"];
@@ -22,4 +29,16 @@ foods.push(39);
 // const randomArr: [string, number, string] = ["random", "array", 30];
 // will be error. because we specified the type of index. so, what should be right code!
 const randomArr = ["random", "array", 30];
+// basic object
+const devs = {
+    name: "marjuk",
+    skilled: false,
+    age: 20,
+};
+// same vabe amra interface diye o type declare kore dite pari.
+const person = {
+    name: "Rokib",
+    age: 24,
+    isMarid: true
+};
 //# sourceMappingURL=index.js.map
--- hello-ts/src/index.ts
+++ hello-ts/src/index.ts
@@ -2,7 +2,7 @@ let firstName = "Ahmad "; //example of implicit typing or Type Inference.
 const lastName: string = "Marjuk"; // example of explicit typing.
 
 // typescript primary types are: string, number, boolian...
-// firstName = 3 this will be an error in typescript.
+// firstName = 3; this will be an error in typescript.
 
 const greets = (name: string, age: number): string => {
   const greetsSent = `Hello! I'm ${name}. I'm ${age} years old.`;
@@ -15,7 +15,15 @@ console.log(funCall);
 // In the case of a function in typescirpt, the data type of the argument must be specified.
 // If desired, you can also specify what it will return.
 // when a function returning nothing, its type will be `void`. But it can run an operation.
+const voidGreets = (name: string, age: number): void => {
+  const greetsSent = `Hello! I'm ${name}. I'm ${age} years old.`;
+  console.log(greetsSent);
+};
+
 // if it typed `never` it will return nothing and it can't run any operation
+function error(message: string): never {
+  throw new Error(message);
+}
 
 // const foods = ["potato", "tomato", "shim", "fish",]
 // foods.push(39) when foods is implicitly string typed, you can't push any data accept string.
@@ -28,3 +36,27 @@ foods.push(39);
 // const randomArr: [string, number, string] = ["random", "array", 30];
 // will be error. because we specified the type of index. so, what should be right code!
 const randomArr: [string, string, number] = ["random", "array", 30];
+
+// basic object
+
+const devs: { name: string; skilled: boolean; age: number; isMarid?: boolean } =
+  {
+    name: "marjuk",
+    skilled: false,
+    age: 20,
+  };
+
+type Person = {
+  name: string;
+  age: number;
+  isMarid: boolean;
+  isSkilled?: boolean;
+};
+
+// same vabe amra interface diye o type declare kore dite pari.
+
+const person: Person = {
+  name: "Rokib",
+  age: 24,
+  isMarid: true
+};
